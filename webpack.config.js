af92abadf0a246801722163dcd3a81c4d849b477
Bucky <buckyroberts@gmail.com>
2016-06-26 18:47:17 -0400

    Removed plugins (want to keep as basic/lightweight as possible)

--- webpack.config.js
+++ webpack.config.js
@@ -24,20 +24,7 @@ module.exports = {
             }
         ]
     },
-    // Allows to check exact errors for a line of source code
-    // Instead of it pointing you to the error in the bundle file
     devtool: 'cheap-module-eval-source-map',
-    plugins: [
-      // Optional: Minify you bundle code.
-     new webpack.optimize.UglifyJsPlugin({
-         compress: {
-             warnings: false,
-         },
-         output: {
-             comments: false,
-         }
-       })
-    ],
     output: {
         path: 'src',
         filename: 'js/bundle.min.js'
