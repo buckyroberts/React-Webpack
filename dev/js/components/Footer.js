54f8325740da7a092d9d68a7231fcce7cb1bb645
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-13 17:56:01 +0800

    Delete dev directory

--- dev/js/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Footer extends React.Component {
-    render() {
-        return (<h2>Footer</h2>);
-    }
-}
--- dev/js/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Header extends React.Component {
-    render() {
-        return (<h1>Header</h1>);
-    }
-}
